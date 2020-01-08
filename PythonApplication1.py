a860f3994e0b933e45fd93b8f1bd125892f09a85
Mohamed Samir <mosamircs@gmail.com>
2020-01-08 16:42:38 +0200

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,44 @@
+print("Hello MOSamirCS")
+
+#integers
+one =1
+two=2
+someNumbers=1000
+
+#Booleans
+true_boolean=True
+false_boolean=False
+
+#Strings
+myName="Mohamed Samir"
+
+#float
+bookPrice=15.80
+
+#Control Flow
+if True:
+	print("Hello Python IF")
+if 2>1:
+	print("2 is greater than 1")
+
+#else condition
+if 1>2: 
+	print("1 is greater than 2")
+else:
+	print("1 is not greater than 2")
+
+#else if (elif)
+if 1>2:
+	print("1 is greater than 2")
+elif 2>1:
+	print ("2 is greater than 1")
+else :
+	print("1 is equal to 2")
+
+#looping and iteration
+num=1
+while num<=10:
+	print(num)
+	num+=1
+#loop condition
+loop_condition=True
